868eff40d08e49d77c67e83c93cb02749e2bb81c
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-09-14 11:08:05 +0900

    .

--- Assets/Script/5/CalcDeg.cs
+++ Assets/Script/5/CalcDeg.cs
@@ -10,8 +10,8 @@ public class CalcDeg : MonoBehaviour
     private (double deg, float angle) CalcDegree()
     {
         //공식 계산
-        double a = Vector3.Distance(bar.transform.position, controlBar.transform.position);
-        a = Math.Round(a, 2);
+        float a = Vector3.Distance(bar.transform.position, controlBar.transform.position);
+        a = (float)Math.Round(a, 2);
         Vector3 barDist = bar.transform.position;
         Vector3 bar2Center = controlBar.transform.GetChild(0).position;
         barDist.x = 0; barDist.z = 0;
@@ -22,24 +22,26 @@ public class CalcDeg : MonoBehaviour
         controlBarDist.x = 0; controlBarDist.y = 0;
         controlBar2Center.x = 0; controlBar2Center.y = 0;
 
-        double b = Vector3.Distance(controlBar.transform.position, controlBar2Center);
-        b = Math.Round(b, 2);
-        double c = Vector3.Distance(bar.transform.position, controlBar2Center);
-        c = Math.Round(c, 2);
+        float b = Vector3.Distance(controlBar.transform.position, controlBar2Center);
+        b = (float)Math.Round(b, 2);
+        float c = Vector3.Distance(bar.transform.position, controlBar2Center);
+        c = (float)Math.Round(c, 2);
 
         //코사인 법칙에 의해
-        double cosTheta = (Mathf.Pow((float)a, 2) - Mathf.Pow((float)b, 2) - Mathf.Pow((float)c, 2)) / -2 * b * c;
-        cosTheta = Math.Round(cosTheta, 2);
+        float cosTheta = (Mathf.Pow(a, 2) - Mathf.Pow(b, 2) - Mathf.Pow(c, 2)) / -2 * b * c;
+        cosTheta = (float)Math.Round(cosTheta, 2);
 
         //Theta값 산출
-        double theta = Mathf.Acos((float)cosTheta);
-        theta = Math.Round(theta, 2);
-        if (double.IsNaN(theta))
+
+        float theta = 1 / Mathf.Cos(cosTheta);
+        Debug.Log(Mathf.Cos((float)cosTheta));
+        theta = (float)Math.Round(theta, 2);
+        if (float.IsNaN(theta))
         {
             theta = 0;
         }
 
-        return (theta * Mathf.Rad2Deg, 0);
+        return (Mathf.Abs(theta * Mathf.Rad2Deg), 0);
     }
     private void Update()
     {
@@ -47,8 +49,9 @@ public class CalcDeg : MonoBehaviour
         while (ang < deg)
         {
             controlBar.transform.RotateAround(controlBar.transform.GetChild(0).position,
-               new Vector3(1, 0, 0), (float)(Time.deltaTime * deg));
-            ang += (float)(Time.deltaTime * deg);
+               new Vector3(1, 0, 0), (float)(Time.deltaTime * deg * 0.1f));
+            ang += (float)(Time.deltaTime * deg * 0.1f);
         }
+
     }
 }
